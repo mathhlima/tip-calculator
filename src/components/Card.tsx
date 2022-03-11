718ae94ab60f79ba3fe0ab9d053636464da98426
Matheus <matheus_sousa@protonmail.com>
2022-03-10 23:29:45 -0300

    style: add styles

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+export default function Card() {
+    return (
+        <div className={styles.mainCard}>
+            <div className={styles.cardLeft}>
+
+            </div>
+            <div className={styles.cardRight}>
+                
+            </div>
+        </div>  
+    );
+}
