85a90328b458325c1b6f79614ad776b68c6412a1
Matheus <matheus_sousa@protonmail.com>
2022-03-12 16:42:23 -0300

    style: add style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,10 +7,45 @@ export default function Card() {
 
             <div className={styles.mainCard}>
                 <div className={styles.cardLeft}>
-        tip
+                    <span className={styles.bill}>Bill</span>
+                    <input className={styles.quantityInput}></input>
+                    <br></br><br></br><br></br>
+
+                    <span className={styles.select}>Select Tip %</span><br></br><br></br>
+
+                    <div className={styles.infoButtons}>
+                        
+                        <button >5%</button>
+                        <button>10%</button>
+                        <button className={styles.percentageButton}>15%</button>
+                        <button>25%</button>
+                        <button>50%</button>
+                        <button className={styles.customButton}>Custom</button>
+                    </div>
+                    <br></br> <br></br> <br></br>
+                    <span className={styles.people}>Number of people</span>
+                    <input  className={styles.quantityPeople}></input>
+
+
                 </div>
                 <div className={styles.cardRight}>
-                    tip
+                    <div className={styles.amount}>
+                        <span>Tip Amount</span>
+                        <span>/ person</span>
+
+                        <span>$4.27</span>
+                    </div>
+
+                    <div className={styles.total}>
+                    <span>Total</span>
+                        <span>/ person</span>
+
+                        <span>$32.79</span>
+                    </div>
+
+                    <div className={styles.resetButton}>
+                        <button>Reset</button>
+                    </div>
                 </div>
             </div>
 
