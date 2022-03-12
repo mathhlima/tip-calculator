bf25d6c05059011913419d25207f2c794ccc21d0
Matheus <matheus_sousa@protonmail.com>
2022-03-11 23:42:06 -0300

    style: add style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,14 +3,18 @@ import styles from './styles.module.scss'
 
 export default function Card() {
     return (
-        <section className={styles.mainCard}>
-            
-            <div className={styles.cardLeft}>
+        <section className={styles.section}>
 
+            <div className={styles.mainCard}>
+                <div className={styles.cardLeft}>
+
+                </div>
+                <div className={styles.cardRight}>
+                    tip
+                </div>
             </div>
-            <div className={styles.cardRight}>
-                
-            </div>
-        </section>  
+
+
+        </section>
     );
 }
